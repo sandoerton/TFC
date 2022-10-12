ed288cff398f275710daa1a20f9b143874491dd3
Sandoerton <sandoerton@gmail.com>
2022-10-12 13:42:37 -0300

    feat: add model users

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,42 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  id?: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Users;
