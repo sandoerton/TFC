0443d113386061df6b024bdcc23cbddf7735b63d
Sandoerton <sandoerton@gmail.com>
2022-10-13 11:46:40 -0300

    fix:remove token options

--- app/backend/src/database/helpers/token.ts
+++ app/backend/src/database/helpers/token.ts
@@ -1,10 +1,10 @@
-import { sign, SignOptions } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'jwt_secret';
-const JWT_OPTIONS: SignOptions = { algorithm: 'HS256', expiresIn: '1d' };
+// const JWT_OPTIONS: SignOptions = { algorithm: 'HS256', expiresIn: '1d' };
 
 const tokenGenerate = (payload: string) => {
-  const token = sign(payload, JWT_SECRET, JWT_OPTIONS);
+  const token = sign(payload, JWT_SECRET);
   return token;
 };
 
